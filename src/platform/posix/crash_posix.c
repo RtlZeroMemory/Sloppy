ed14baca1293d3aa1bd6cd7572068b5844bd7bff
Rez0xFF <rezo@scrubchart.ai>
2026-05-12 14:35:47 +0400

    Use controlled POSIX fatal exit

--- src/platform/posix/crash_posix.c
+++ src/platform/posix/crash_posix.c
@@ -1,13 +1,12 @@
 #include "sloppy/platform_crash.h"
 
-#include <stdlib.h>
 #include <unistd.h>
 
 void sl_platform_disable_interactive_crash_ui(void) {}
 
 void sl_platform_abort_process(void)
 {
-    abort();
+    _Exit(3);
 }
 
 uint64_t sl_platform_process_id(void)
